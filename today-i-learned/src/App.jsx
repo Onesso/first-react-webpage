bb8a494231e4dc7ee8ddf3e7a92e105751e12fc4
Frank Onesso <odhiambofrank1965@gmail.com>
2023-09-08 14:10:27 +0300

    created all of the main components of the UI

--- today-i-learned/src/App.jsx
+++ today-i-learned/src/App.jsx
@@ -2,15 +2,34 @@ import "./style.css";
 
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" alt="today_i_learned_logo" />
-        <h1 className="h1">Today I learned</h1>
-      </div>
+    <>
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="today_i_learned_logo" />
+          <h1 className="h1">Today I learned</h1>
+        </div>
 
-      <button className="btn btn-large btn-open">share a fact</button>
-    </header>
+        <button className="btn btn-large btn-open">share a fact</button>
+      </header>
+      <NewFactForm />
+
+      <main className="main">
+        <CategoryFilters />
+        <FactList />
+      </main>
+    </>
   );
 }
 
+function NewFactForm() {
+  return <form className="fact-form">Fact Form</form>;
+}
+
+function CategoryFilters() {
+  return <aside> Category filters</aside>;
+}
+
+function FactList() {
+  return <section> fact-list </section>;
+}
 export default App;
