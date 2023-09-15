06fe412b71d6592956687ee3e8183da75fd8709b
Frank Onesso <odhiambofrank1965@gmail.com>
2023-09-15 12:21:55 +0300

    feched data from the supase, created a loader

--- today-i-learned/src/App.jsx
+++ today-i-learned/src/App.jsx
@@ -1,51 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import supabase from "./supabase";
 import "./style.css";
 
-//this is some external data hard coded for the purpose of learning
-
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-  {
-    id: 4,
-    text: "React, sometimes referred to as a frontend JavaScript framework, is a JavaScript library created by Facebook.React is a tool for building UI components.",
-    source: "https://www.w3schools.com/REACT/react_intro.asp",
-    category: "technology",
-    votesInteresting: 9,
-    votesMindblowing: 5,
-    votesFalse: 1,
-    createdIn: 2011,
-  },
-];
 const CATEGORIES = [
   { name: "technology", color: "#3b82f6" },
   { name: "science", color: "#16a34a" },
@@ -72,7 +28,22 @@ function App() {
   }
 
   const [ShowForm, setShowForm] = useState(false);
-  const [facts, setfacts] = useState(initialFacts);
+  const [facts, setfacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  //the empty array in the function in the useEffect will ensure that the function only runs once, as soon the first component renders
+  useEffect(function () {
+    {
+      /*supabase take time to load the data so we must wait and wait is only used in an async function therefore the async is created inside the function of the useState */
+    }
+    async function getFacts() {
+      setIsLoading(true);
+      const { data: facts, error } = await supabase.from("facts").select("*");
+      setfacts(facts);
+      setIsLoading(false);
+    }
+    getFacts();
+  }, []);
 
   return (
     <>
@@ -88,10 +59,14 @@ function App() {
 
       <main className="main">
         <CategoryFilters />
-        <FactList facts={facts} />
+        {isLoading ? <Loader /> : <FactList facts={facts} />}
       </main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loaderMessage"> Loading... </p>;
 }
 
 function Header({ ShowForm, setShowForm }) {
